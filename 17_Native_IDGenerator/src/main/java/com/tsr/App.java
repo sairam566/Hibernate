1167141909dda48c15f6585674f02dd26b1f0f1d
sairam566 <sairam1431995@gmail.com>
2023-02-07 11:04:46 +0530

    Hibernate Native ID Generator

---
+++ 17_Native_IDGenerator/src/main/java/com/tsr/App.java
@@ -0,0 +1,29 @@
+package com.tsr;
+
+import java.util.Date;
+
+import com.tsr.dao.EmployeeDAO;
+import com.tsr.entityes.Employee;
+import com.tsr.helper.SessionFactoryRegistory;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	EmployeeDAO dao = null;
+    	Employee employee = null;
+    	try {
+			dao = new EmployeeDAO();
+			employee = new Employee();
+			employee.setEmployeeName("TSR");
+			employee.setEmployeeDept("CEO");
+			employee.setEmployeeDOJ(new Date(2025, 10, 01));
+			employee.setEmployeeSalary(120000000.0);
+			int pkVal = dao.addEmployee(employee);
+			System.out.println("The Primary Key Value is: "+pkVal);
+			System.out.println(employee);
+		} finally {
+			SessionFactoryRegistory.closeSessionFactory();
+		}
+    }
+}
---
+++ 17_Native_IDGenerator/src/main/java/com/tsr/entityes/Employee.java
@@ -0,0 +1,64 @@
+package com.tsr.entityes;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Employee implements Serializable{
+	
+	protected int employeeNo;
+	protected String employeeName;
+	protected String employeeDept;
+	protected Date employeeDOJ;
+	protected Double employeeSalary;
+	
+	public Employee() {
+		super();
+	}
+
+	public int getEmployeeNo() {
+		return employeeNo;
+	}
+
+	public void setEmployeeNo(int employeeNo) {
+		this.employeeNo = employeeNo;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public String getEmployeeDept() {
+		return employeeDept;
+	}
+
+	public void setEmployeeDept(String employeeDept) {
+		this.employeeDept = employeeDept;
+	}
+
+	public Date getEmployeeDOJ() {
+		return employeeDOJ;
+	}
+
+	public void setEmployeeDOJ(Date employeeDOJ) {
+		this.employeeDOJ = employeeDOJ;
+	}
+
+	public Double getEmployeeSalary() {
+		return employeeSalary;
+	}
+
+	public void setEmployeeSalary(Double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeNo=" + employeeNo + ", employeeName=" + employeeName + ", employeeDept="
+				+ employeeDept + ", employeeDOJ=" + employeeDOJ + ", employeeSalary=" + employeeSalary + "]";
+	}
+	
+}
