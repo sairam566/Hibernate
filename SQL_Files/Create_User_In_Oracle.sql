3ac22934d9b84ec523067fb9b1a4643d5ae39753
sairam566 <sairam1431995@gmail.com>
2023-02-08 15:03:13 +0530

    Create User in Oracle and Grant him/her permissions

---
+++ SQL_Files/Create_User_In_Oracle.sql
@@ -0,0 +1,5 @@
+select * from scott.emp;
+
+create user sairam identified by sairam;
+grant connect to sairam;
+grant all privileges to sairam identified by sairam;
---
+++ SQL_Files/Sairam_DB.sql
@@ -0,0 +1,4 @@
+select * from employee;
+select employee_seq.nextval from dual;
+delete from employee;
+desc employee;
