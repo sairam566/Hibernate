393da9b4199b423b9543482a99e089b24d090b8d
sairam566 <sairam1431995@gmail.com>
2023-02-10 12:20:37 +0530

    JPA Identity Sequence Generator

---
+++ 23_Identity_JPA_IDGenerator/src/main/java/com/tsr/entityes/Bus.java
@@ -0,0 +1,91 @@
+package com.tsr.entityes;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bus")
+public class Bus implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "bus_no")
+	protected int busNo;
+	
+	@Column(name = "bus_type")
+	protected String busType;
+	
+	@Column(name = "REGISTRATION_NO")
+	protected String registrationNo;
+	
+	@Column(name = "FUEL_TYPE")
+	protected String fuelType;
+	
+	//No need to mention @column as both the column name and attribute name matches
+	protected String color;
+	protected int capacity;
+	
+	public Bus() {
+		super();
+	}
+
+	public int getBusNo() {
+		return busNo;
+	}
+
+	public void setBusNo(int busNo) {
+		this.busNo = busNo;
+	}
+
+	public String getBusType() {
+		return busType;
+	}
+
+	public void setBusType(String busType) {
+		this.busType = busType;
+	}
+
+	public String getRegistrationNo() {
+		return registrationNo;
+	}
+
+	public void setRegistrationNo(String registrationNo) {
+		this.registrationNo = registrationNo;
+	}
+
+	public String getFuelType() {
+		return fuelType;
+	}
+
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	@Override
+	public String toString() {
+		return "Bus [busNo=" + busNo + ", busType=" + busType + ", registrationNo=" + registrationNo + ", fuelType="
+				+ fuelType + ", color=" + color + ", capacity=" + capacity + "]";
+	}
+}
