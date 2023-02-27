e73aedd805b3dcd05519b99e31d1ed87ec5ebef0
sairam566 <sairam1431995@gmail.com>
2023-02-27 10:03:57 +0530

    Entity Classes

---
+++ 35_JPA_Annotation_JOINED/src/main/java/com/tsr/entities/CardPayment.java
@@ -0,0 +1,74 @@
+package com.tsr.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+@Entity
+@PrimaryKeyJoinColumn
+public class CardPayment extends Payment{
+	
+	@Column(name = "card_no")
+	protected String cardNo;
+	@Column(name = "card_holder_nm")
+	protected String cardHolderName;
+	@Column(name = "issued_bank")
+	protected String issuedBank;
+	@Column(name = "expiry_day")
+	protected int expiryDay;
+	@Column(name = "expiry_month")
+	protected int expiryMonth;
+	
+	public CardPayment() {
+		super();
+	}
+
+	public String getCardNo() {
+		return cardNo;
+	}
+
+	public void setCardNo(String cardNo) {
+		this.cardNo = cardNo;
+	}
+
+	public String getCardHolderName() {
+		return cardHolderName;
+	}
+
+	public void setCardHolderName(String cardHolderName) {
+		this.cardHolderName = cardHolderName;
+	}
+
+	public String getIssuedBank() {
+		return issuedBank;
+	}
+
+	public void setIssuedBank(String issuedBank) {
+		this.issuedBank = issuedBank;
+	}
+
+	public int getExpiryDay() {
+		return expiryDay;
+	}
+
+	public void setExpiryDay(int expiryDay) {
+		this.expiryDay = expiryDay;
+	}
+
+	public int getExpiryMonth() {
+		return expiryMonth;
+	}
+
+	public void setExpiryMonth(int expiryMonth) {
+		this.expiryMonth = expiryMonth;
+	}
+
+	@Override
+	public String toString() {
+		return "CardPayment [cardNo=" + cardNo + ", cardHolderName=" + cardHolderName + ", issuedBank=" + issuedBank
+				+ ", expiryDay=" + expiryDay + ", expiryMonth=" + expiryMonth + ", paymentNo=" + paymentNo
+				+ ", description=" + description + ", customerName=" + customerName + ", paymentDate=" + paymentDate
+				+ ", paidAmount=" + paidAmount + "]";
+	}
+	
+}
---
+++ 35_JPA_Annotation_JOINED/src/main/java/com/tsr/entities/ChequePayment.java
@@ -0,0 +1,75 @@
+package com.tsr.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+@Entity
+@PrimaryKeyJoinColumn
+public class ChequePayment extends Payment {
+	@Column(name = "cheque_no")
+	protected String chequeNo;
+	@Column(name = "bank_nm")
+	protected String bankName;
+	@Column(name = "branch_nm")
+	protected String branchName;
+	@Column(name = "payee_nm")
+	protected String payeeName;
+	@Column(name = "cheque_dt")
+	protected Date chequeDate;
+	
+	public ChequePayment() {
+		super();
+	}
+
+	public String getChequeNo() {
+		return chequeNo;
+	}
+
+	public void setChequeNo(String chequeNo) {
+		this.chequeNo = chequeNo;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public String getBranchName() {
+		return branchName;
+	}
+
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+
+	public String getPayeeName() {
+		return payeeName;
+	}
+
+	public void setPayeeName(String payeeName) {
+		this.payeeName = payeeName;
+	}
+
+	public Date getChequeDate() {
+		return chequeDate;
+	}
+
+	public void setChequeDate(Date chequeDate) {
+		this.chequeDate = chequeDate;
+	}
+
+	@Override
+	public String toString() {
+		return "ChequePayment [chequeNo=" + chequeNo + ", bankName=" + bankName + ", branchName=" + branchName
+				+ ", payeeName=" + payeeName + ", chequeDate=" + chequeDate + ", paymentNo=" + paymentNo
+				+ ", description=" + description + ", customerName=" + customerName + ", paymentDate=" + paymentDate
+				+ ", paidAmount=" + paidAmount + "]";
+	}
+	
+}
