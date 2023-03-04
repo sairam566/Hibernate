212b9274dd22e9957728e05e05288a67ff1744cc
sairam566 <sairam1431995@gmail.com>
2023-03-04 08:43:29 +0530

    Entity classes

---
+++ 36_JPA_Annotation_TABLE_PER_CLASS/src/main/java/com/tsr/entities/Payment.java
@@ -0,0 +1,83 @@
+package com.tsr.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@GenericGenerator(name = "incrementGen", strategy = "increment")
+public class Payment implements Serializable{
+	
+	@Id
+	@Column(name = "payment_no")
+	@GeneratedValue(generator = "incrementGen")
+	protected int paymentNo;
+	
+	protected String description;
+	@Column(name = "customer_name")
+	protected String customerName;
+	@Column(name = "payment_date")
+	protected Date paymentDate;
+	@Column(name = "paid_amt")
+	protected double paidAmount;
+	
+	public Payment() {
+		super();
+	}
+	
+	public int getPaymentNo() {
+		return paymentNo;
+	}
+
+	public void setPaymentNo(int paymentNo) {
+		this.paymentNo = paymentNo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public Date getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(Date paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public double getPaidAmount() {
+		return paidAmount;
+	}
+
+	public void setPaidAmount(double paidAmount) {
+		this.paidAmount = paidAmount;
+	}
+
+	@Override
+	public String toString() {
+		return "Payment [paymentNo=" + paymentNo + ", description=" + description + ", customerName=" + customerName
+				+ ", paymentDate=" + paymentDate + ", paidAmount=" + paidAmount + "]";
+	}
+	
+}
