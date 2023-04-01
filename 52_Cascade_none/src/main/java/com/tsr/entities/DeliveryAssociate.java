94fdc10b208a75d9fd6ab81344d6cd6393ca98d6
sairam566 <sairam1431995@gmail.com>
2023-04-01 17:55:46 +0530

    Entity classes

---
+++ 52_Cascade_none/src/main/java/com/tsr/entities/DeliveryAssociate.java
@@ -0,0 +1,26 @@
+package com.tsr.entities;
+
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = {"parcels"})
+@Builder
+public class DeliveryAssociate {
+	
+	protected int deliveryAssociateNo;
+	protected String associateName;
+	protected int experience;
+	protected String designation;
+	protected String contactNo;
+	protected Set<Parcel> parcels;
+}
