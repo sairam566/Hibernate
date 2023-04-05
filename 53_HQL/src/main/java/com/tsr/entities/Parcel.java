8081b64ff501aa67aeafc6304edb9d4f5df0e81d
sairam566 <sairam1431995@gmail.com>
2023-04-05 10:50:11 +0530

    Entity classes

---
+++ 53_HQL/src/main/java/com/tsr/entities/Parcel.java
@@ -0,0 +1,26 @@
+package com.tsr.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = {"deliveryAssociate"})
+@Builder
+public class Parcel implements Serializable{
+	
+	protected int parcelNo;
+	protected String description;
+	protected String deliveryType;
+	protected int weight;
+	protected double charges;
+	protected DeliveryAssociate deliveryAssociate;
+}
